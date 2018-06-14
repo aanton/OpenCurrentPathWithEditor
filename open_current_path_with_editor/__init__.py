08ecdd4d1f9998f53a7063b1fa97b485b74be551
James Sully <sullyj3@gmail.com>
2018-06-14 15:20:10 +1000

    Fix outdated plugin API

--- open_current_path_with_editor/__init__.py
+++ open_current_path_with_editor/__init__.py
@@ -1,12 +1,13 @@
 from core.commands import OpenWithEditor
 from fman import show_alert
+from fman.url import as_human_readable
 from os.path import exists
 
 class OpenCurrentPathWithEditor(OpenWithEditor):
     def __call__(self):
         path_to_open = self.pane.get_path()
 
-        if exists(path_to_open):
-            self._open_with_editor(path_to_open)
+        if exists(as_human_readable(path_to_open)):
+            super().__call__(path_to_open)
         else:
             show_alert('No current path detected!')
